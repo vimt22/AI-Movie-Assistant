b2d80e5110c3945ff470e6362b9a3072f474f342
vimt22 <77280219+vimt22@users.noreply.github.com>
2024-03-18 20:18:31 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
+  title: 'AI Movie Assistant',
+  description: 'Find Your Next Movie In Seconds.',
+  copyright: 'Revolxa Inc',
+  privacy_policy: 'https://myappterms.com/8RNC8K/1/en',
+  default_language: 'en-US',
 }
 
 export const isShowPrompt = false
